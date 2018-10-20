94ef102ac3d2dcd5b8a3435ed66a4b455d847670
carlotacb <carkbra@gmail.com>
2018-10-20 23:30:56 +0200

    Server UO

--- server/index.js
+++ server/index.js
@@ -1,16 +1,34 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const config = require('./config')
-const BootstrapRouter = require('./routes')
- BootstrapRouter(app)
-BootstrapServer(app)
-StartServer(app)
- function BootstrapServer(app) {
-	app.use(bodyParser.json())
-}
- function StartServer(app) {
-	app.listen(config.port, function() {
-		console.log(`Agora app listening on port ${config.port}!`)
-	})
-}
+let express = require('express')
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+let app = express();
+
+let apiRoutes = require("./api-routes")
+
+// Configure bodyparser to handle post requests
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
+app.use(bodyParser.json());
+
+// Connect to Mongoose and set connection variable
+mongoose.connect('mongodb://localhost/resthub');
+
+var db = mongoose.connection;
+// Setup server port
+var port = process.env.PORT || 8080;
+
+// Send message for default URL
+app.get('/', (req, res) => res.send('Hello World with Express'));
+
+// Use API routes in the App
+app.use('/api', apiRoutes);
+
+// Launch app to listen to specified port
+app.listen(port, function () {
+     console.log("Running RestHub on port " + port);
+});
+
+
+
+
