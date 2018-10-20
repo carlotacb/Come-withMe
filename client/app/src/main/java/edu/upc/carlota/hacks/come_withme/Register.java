87d89f7160948fc9481540fabf15fbbb1086678a
Luis <luis@MacBook-Pro-de-Luis.local>
2018-10-20 17:04:46 +0200

    finished checkboxes weekdays

--- client/app/src/main/java/edu/upc/carlota/hacks/come_withme/Register.java
+++ client/app/src/main/java/edu/upc/carlota/hacks/come_withme/Register.java
@@ -1,5 +1,7 @@
 package edu.upc.carlota.hacks.come_withme;
 
+import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,20 +13,27 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.Arrays;
 
 public class Register extends AppCompatActivity {
 
     private EditText etName, etPassword, etMail;
     private Button login;
     private String nameRegister, password, emailRegister;
+    private Boolean[] weekDays;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        this.weekDays = new Boolean [5];
+        Arrays.fill(weekDays, false);
 
         etName = (EditText) findViewById(R.id.nameRegister);
         etPassword = (EditText) findViewById(R.id.password);
@@ -49,7 +58,7 @@ public class Register extends AppCompatActivity {
 
         Spinner dynamicSpinner = (Spinner) findViewById(R.id.dynamic_spinner);
 
-        String[] items = new String[] { "Chai Latte", "Green Tea", "Black Tea" };
+        String[] items = new String[] { "Female", "Male", "Other" };
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, items);
@@ -68,6 +77,56 @@ public class Register extends AppCompatActivity {
                 // TODO Auto-generated method stub
             }
         });
+
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Register.this, Preferences.class));
+            }
+        });
+    }
+
+    //CHECKBOXES
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.checkbox_monday:
+                if (checked)
+                weekDays[0] = true;
+            else
+                // No Mondays
+                break;
+            case R.id.checkbox_tuesday:
+                if (checked)
+                    weekDays[1] = true;
+            else
+                // No Tuesdays
+                break;
+            case R.id.checkbox_wednesday:
+                if (checked)
+                    weekDays[2] = true;
+            else
+                // No Wednesdays
+                break;
+            case R.id.checkbox_thursday:
+                if (checked)
+                    weekDays[3] = true;
+            else
+                // No Thrusdays
+                break;
+            case R.id.checkbox_friday:
+                if (checked)
+                    weekDays[4] = true;
+            else
+                // No Fridays
+                break;
+            // TODO: Day of the week
+        }
     }
 
 }
