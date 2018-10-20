e2a3b7871e8d9070aab0d3d456dff4947388cd87
carlotacb <carkbra@gmail.com>
2018-10-20 03:46:29 +0200

    Login start

--- client/app/src/main/java/edu/upc/carlota/hacks/come_withme/MainActivity.java
+++ client/app/src/main/java/edu/upc/carlota/hacks/come_withme/MainActivity.java
@@ -1,13 +1,47 @@
 package edu.upc.carlota.hacks.come_withme;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etUsername, etPassword;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUsername = (EditText) findViewById(R.id.username);
+        etPassword = (EditText) findViewById(R.id.password);
+
+        login = (Button) findViewById(R.id.btnLogin);
+        TextView register = (TextView) findViewById(R.id.btnRegister);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Register.class));
+            }
+        });
+
     }
+
+
 }
