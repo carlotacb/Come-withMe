2935162c70de0c5cc6f442fe249da36156b8b459
ericmba <emartinaviles@gmail.com>
2018-10-20 21:21:09 +0200

    navigation creado

--- client/app/src/main/java/edu/upc/carlota/hacks/come_withme/MainActivity.java
+++ client/app/src/main/java/edu/upc/carlota/hacks/come_withme/MainActivity.java
@@ -3,10 +3,13 @@ package edu.upc.carlota.hacks.come_withme;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.PorterDuff;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private TextInputLayout errorUsername, errorPassword;
     private Button login;
     private String username, password;
+
 
     private Resources res = this.getResources();
 
